bc129971262680e34233f29b61b34e16259479f3
eswarclynn <esuclintblazer@gmail.com>
2020-07-30 01:28:20 +0530

    Integrated using webpack

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import './App.css';
 
 import MainContainer from './components/MainContainer'
 import { BrowserRouter as Router } from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
